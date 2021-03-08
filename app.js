115d257c8bc72a64173ba7db4fae42786479ce55
Sahil Deshmukh <thesahildeshmukh@gmail.com>
2021-03-07 21:26:53 -0800

    Working state

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const Jimp = require("jimp");
 const fileUpload = require("express-fileupload");
 const imgProcessor = require(`${__dirname}/img-processor.js`);
 
