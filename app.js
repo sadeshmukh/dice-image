c56ff02f60de255e5234feab18eb9b2fea4099c2
Sahil Deshmukh <thesahildeshmukh@gmail.com>
2021-03-09 15:09:36 -0800

    Random image select, server-side and client-side cache

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const imgProcessor = require(`${__dirname}/img-processor.js`);
-
+const fs = require("fs");
+const imageCache = {};
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
@@ -20,7 +21,29 @@ app.get("/", function (req, res) {
 });
 
 app.get("/new", function (req, res) {
-  res.render("index2", { imageArray: [] });
+  allFiles = fs.readdirSync("public/images/samples");
+
+  allFiles.forEach(function (val, i) {
+    allFiles[i] = allFiles[i].slice(0, -4);
+  });
+
+  console.log(allFiles);
+  const countFiles = allFiles.length;
+  let sampleImages = [];
+
+  i = 0;
+
+  while (sampleImages.length < 3) {
+    let randomImage = allFiles[Math.floor(Math.random() * countFiles)];
+    if (!sampleImages.includes(randomImage) && randomImage.charAt(0) != ".") {
+      sampleImages.push(randomImage);
+      // console.log(randomImage);
+    }
+    i++;
+  }
+
+  console.log(sampleImages);
+  res.render("index2", { imageArray: [], sampleImages: sampleImages });
 });
 
 // app.get("/results/:path", function (req, res) {
@@ -28,10 +51,17 @@ app.get("/new", function (req, res) {
 // });
 
 app.get("/results/:image", function (req, res) {
+  if (imageCache[req.params.image]) {
+    res.send(imageCache[req.params.image]);
+    console.log(imageCache);
+    return;
+  }
+
   imgProcessor.dither(
     `${__dirname}/public/images/samples/${req.params.image}.jpg`,
     (err, imgArray) => {
       console.log("callback called back");
+      imageCache[req.params.image] = imgArray;
       res.send(imgArray);
     }
   );
